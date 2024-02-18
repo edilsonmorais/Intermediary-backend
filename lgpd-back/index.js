a6a2464a4edd4f02f17b4ca988a982763a747fae
Edilson <edilsonmorais16@gmail.com>
2024-02-18 20:45:56 -0300

    Adicionando alterações no backend

--- lgpd-back/index.js
+++ lgpd-back/index.js
@@ -5,11 +5,15 @@ import {router} from "./routes/router.js";
 import sequelize from "./utils/database.js";
 import association from "./models/association.js";
 
+import cors from "cors";
+
 const app = express();
 const {json, urlendcoded}  = pkg;
 
 app.use(json());
 app.use(urlendcoded({extended: true}));
+app.use("/", router);
+app.use(cors());
 
 (async () =>{
     try{
@@ -17,11 +21,10 @@ app.use(urlendcoded({extended: true}));
         await sequelize.sync();
         app.listen(3000, function() {
             console.log('Listening from 3000');
-        });
-        
+        });        
     } catch(error){
         console.log(error);
     }
 })();
 
-app.use("/", router);
+// app.use("/", router);
